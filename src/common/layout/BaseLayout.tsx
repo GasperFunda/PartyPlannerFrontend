ba5c3c68b32ea1689086a135b4cb17d143dbfd80
Gašper Funda Povše <gasper.fp@gmail.com>
2022-03-10 19:06:33 +0100

    Added circles to background

--- src/common/layout/BaseLayout.tsx
+++ src/common/layout/BaseLayout.tsx
@@ -11,37 +11,43 @@ interface IProps {
 
 export default function BaseLayout({ children }: IProps): ReactElement {
   return (
-    <Layout>
-      <div className="page-container">
-        <Row align="top">
-          <Col
-            xs={24}
-            sm={24}
-            md={24}
-            lg={24}
-            xl={24}
-            xxl={24}
-            className="blackBg"
-          >
-            <Header />
-          </Col>
-        </Row>
+    <>
+      <div className="circle1"></div>
+      <div className="circle2"></div>
+      <div className="circle3"></div>
+      <div className="circle4"></div>
+      <Layout>
+        <div className="page-container">
+          <Row align="top">
+            <Col
+              xs={24}
+              sm={24}
+              md={24}
+              lg={24}
+              xl={24}
+              xxl={24}
+              className="blackBg"
+            >
+              <Header />
+            </Col>
+          </Row>
 
-        <Row style={{ height: "100%" }}>
+          <Row style={{ height: "100%", zIndex: 4 }}>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+              <Content>{children}</Content>
+            </Col>
+          </Row>
+        </div>
+        <Row align="bottom">
           <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
-            <Content>{children}</Content>
+            <Footer className="footer blackBg">
+              <p style={{ color: "white" }}>
+                Copyright © {new Date().getFullYear()} Party Planner
+              </p>
+            </Footer>
           </Col>
         </Row>
-      </div>
-      <Row align="bottom">
-        <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
-          <Footer className="footer blackBg">
-            <p style={{ color: "white" }}>
-              Copyright © {new Date().getFullYear()} Party Planner
-            </p>
-          </Footer>
-        </Col>
-      </Row>
-    </Layout>
+      </Layout>
+    </>
   );
 }
