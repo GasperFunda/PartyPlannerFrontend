d4dda7f27f862c2f69c328c8203a344162ec848d
Gašper Funda Povše <gasper.fp@gmail.com>
2022-03-13 12:05:57 +0100

    Integrated things into backend

--- src/types/party.tsx
+++ src/types/party.tsx
@@ -2,4 +2,30 @@ export interface PartyFormRequest {
   title: string;
   description: string;
   location: string;
+  private: boolean;
+  date: moment.Moment;
+  time: moment.Moment;
+}
+
+export interface CreatePartyRequest {
+  partyName: string;
+  location: string;
+  private: boolean;
+  image: string;
+  likes: number;
+  description: string;
+  dateTime: string;
+  fk_user_host: string | null;
+}
+
+export interface PartyResponse {
+  _id: string;
+  partyName: string;
+  location: string;
+  private: boolean;
+  image: string;
+  likes: number;
+  description: string;
+  dateTime: string;
+  fk_user_host: string | null;
 }
