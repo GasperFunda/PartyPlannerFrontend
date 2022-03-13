46d488ad76552ff27b112b1d6dcf0187d96cb564
Gašper Funda Povše <gasper.fp@gmail.com>
2022-03-13 08:32:48 +0100

    Integrated login and register

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,23 @@
-import { ReactElement } from "react";
+import { message } from "antd";
+import axios from "axios";
+import { ReactElement, useCallback } from "react";
+import { UserLogin } from "../../types/user";
 import Form from "./Form";
 
 export default function Login(): ReactElement {
+  const handleLoginUser = useCallback((user: UserLogin) => {
+    axios.post(`${process.env.REACT_APP_API_URL}/login`, user).then((res) => {
+      localStorage.setItem("userID", res.data._id);
+      localStorage.setItem("username", res.data.username);
+      message.success("Login successful!");
+      window.location.href = "/home";
+    });
+  }, []);
+
   return (
     <div className="loginContainer">
       <div className="signInBody">
-        <Form />
+        <Form onFinish={handleLoginUser} />
       </div>
     </div>
   );
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,11 +1,28 @@
-import { ReactElement } from "react";
+import { message } from "antd";
+import axios from "axios";
+import { ReactElement, useCallback } from "react";
+import { UserRegister } from "../../types/user";
 import Form from "./Form";
 
 export default function Register(): ReactElement {
+  const handleRegisterUser = useCallback((user: UserRegister) => {
+    delete user.repeatPassword;
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/users/register`, user)
+      .then(() => {
+        axios
+          .get(`${process.env.REACT_APP_API_URL}/users/${user.username}`)
+          .then((res) => {
+            message.success("Register successful!");
+            window.location.href = "/login";
+          });
+      });
+  }, []);
+
   return (
     <div className="loginContainer">
       <div className="registerBody">
-        <Form />
+        <Form onFinish={handleRegisterUser} />
       </div>
     </div>
   );
