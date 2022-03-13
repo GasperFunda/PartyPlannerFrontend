cbc589883473111f9fa45afea5a54f5cfb2861cb
Gašper Funda Povše <gasper.fp@gmail.com>
2022-03-13 14:42:24 +0100

    Created edit profile page and intergrated with BE

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,15 +1,38 @@
 import { Button, Divider } from "antd";
+import axios from "axios";
 import * as React from "react";
+import { useEffect, useState } from "react";
+import { User } from "../../types/user";
 
 export default function Profile() {
+  const [user, setUser] = useState({} as User);
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/users/id/${localStorage.getItem(
+          "userID"
+        )}`
+      )
+      .then((res) => {
+        console.log(res.data);
+
+        setUser(res.data);
+      });
+  }, []);
   return (
     <div className="createPartyContainer">
       <img src="profilepicsample.png" alt="profilepic" width={"150px"} />
-      <h2>John Doe</h2>
+      <h2>
+        {user.name} {user.surname}
+      </h2>
       <hr style={{ width: "250px" }} />
-      <h2>Parties held: 3</h2>
-      <h2>Parties attended: 15</h2>
-      <Button className="button" id="editProfileButton">
+      <h2>Parties held: 1</h2>
+      <h2>Parties attended: 2</h2>
+      <Button
+        className="button"
+        id="editProfileButton"
+        onClick={() => (window.location.href = "/edit-profile")}
+      >
         CHANGE USER PROFILE
       </Button>
     </div>
--- src/types/user.tsx
+++ src/types/user.tsx
@@ -1,20 +1,27 @@
 export class User {
-    id: number;
-    name: string;
-    surname: string;
-    email: string;
-    attendanceStatus: number
-    picture: string
+  id: number;
+  name: string;
+  surname: string;
+  email: string;
+  attendanceStatus: number;
+  picture: string;
 
-    constructor(id: number, name: string, surname: string, email: string, attendanceStatus: number, picture: string) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.attendanceStatus = attendanceStatus;
-        this.picture = picture;
-    }
-}  
+  constructor(
+    id: number,
+    name: string,
+    surname: string,
+    email: string,
+    attendanceStatus: number,
+    picture: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.attendanceStatus = attendanceStatus;
+    this.picture = picture;
+  }
+}
 export interface UserRegister {
   name: string;
   surname: string;
@@ -23,6 +30,8 @@ export interface UserRegister {
   password: string;
   repeatPassword?: string;
 }
+
+export type IUser = Omit<UserRegister, "repeatPassword">;
 
 export interface UserLogin {
   username: string;
